ff6d645f66bff15d782c3ac242602406e6190689
hsin86 <kxin666@gmail.com>
2020-05-29 20:13:05 +0800

    add project

---
+++ Color Generator/README.md
@@ -0,0 +1,17 @@
+# Color Generator
+
+## Project Point:
+
+1. Input the rgb value can change three place color
+2. Use different listener to catch input value
+3. Can use random button get random color
+4. Click copy button can get color rgb value
+5. [Project Demo](https://day-project.zkhsin.now.sh/Color%20Generator/)
+
+---
+
+## Project Use:
+
+- HTML
+- CSS
+- Javascript
---
+++ Color Generator/script.js
@@ -0,0 +1,38 @@
+const color = document.querySelector("input");
+const show = document.querySelector(".color-show");
+const random = document.querySelector(".random");
+const copy = document.querySelector(".copy");
+const title = document.querySelector("h3");
+
+function showColor() {
+  show.style.backgroundColor = `#${color.value}`;
+  title.style.backgroundColor = `#${color.value}`;
+}
+
+function changeBackground() {
+  document.body.style.backgroundColor = `#${color.value}`;
+}
+
+function randomColor() {
+  let setColor = `${Math.floor(Math.random() * 16777215).toString(16)}`;
+  color.value = setColor;
+  showColor();
+  changeBackground();
+}
+
+function copyColor() {
+  let value = color.value;
+  let text = document.createElement("input");
+  text.style.textTransform = "uppercase";
+  text.setAttribute("value", `#${value}`);
+  document.body.appendChild(text);
+  text.select();
+  document.execCommand("copy");
+  text.remove();
+  alert("Color is already copied");
+}
+
+color.addEventListener("change", changeBackground);
+color.addEventListener("keyup", showColor);
+random.addEventListener("click", randomColor);
+copy.addEventListener("click", copyColor);
